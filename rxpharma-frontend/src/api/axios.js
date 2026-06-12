b0e58533efd970d9e23625ed2a8225d13eb4216c
BIRUK-MULATU <birukmulatukibret@gmail.com>
2026-06-12 10:47:05 +0300

    we publish the ci/cd part on out project

--- rxpharma-frontend/src/api/axios.js
+++ rxpharma-frontend/src/api/axios.js
@@ -1,26 +1,20 @@
 import axios from 'axios'
-import { API_BASE_URL } from '../utils/constants'
 
 const api = axios.create({
-  baseURL: API_BASE_URL,
+  baseURL: import.meta.env.VITE_API_URL || 'http://localhost:8083',
   headers: {
     'Content-Type': 'application/json',
   },
 })
 
-// Add token to every request
-api.interceptors.request.use(
-  (config) => {
-    const token = localStorage.getItem('token')
-    if (token) {
-      config.headers.Authorization = `Bearer ${token}`
-    }
-    return config
-  },
-  (error) => Promise.reject(error)
-)
+api.interceptors.request.use((config) => {
+  const token = localStorage.getItem('token')
+  if (token) {
+    config.headers.Authorization = `Bearer ${token}`
+  }
+  return config
+})
 
-// Handle 401 globally
 api.interceptors.response.use(
   (response) => response,
   (error) => {
