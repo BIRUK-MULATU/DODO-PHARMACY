e09bd756a51889d0efae029d2a721ea24e8ea1f2
BIRUK-MULATU <birukmulatukibret@gmail.com>
2026-06-29 15:03:21 +0300

    add shaddow on the logo

--- rxpharma-frontend/src/pages/auth/LoginPage.jsx
+++ rxpharma-frontend/src/pages/auth/LoginPage.jsx
@@ -43,7 +43,7 @@ export default function LoginPage() {
 
         {/* Logo */}
       <div className="text-center mb-8">
-        <div className="inline-flex items-center justify-center w-70 h-70 bg-gradient-to-br from-accent-500 to-accent-600 rounded-2xl mb-4 shadow-lg overflow-hidden">
+        <div className="inline-flex items-center justify-center w-70 h-70 bg-gradient-to-br from-accent-500 to-accent-600 rounded-2xl mb-4 shadow-2xl overflow-hidden">
           <img
             src="/image.svg"
             alt="DODO PHARMACY Logo"
